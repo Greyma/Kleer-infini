6f6917d06c808e0782ff9db4a5d3dab801bc8c8f
Grey <grey.lanisteur123@gmail.com>
2025-09-13 06:48:12 +0100

    CORS

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(compression());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? ['https://garoui-electricite.com'] 
-    : ['http://localhost:3000', 'http://localhost:3001','http://localhost:5173'],
+    : ['*', 'http://localhost:3001','http://localhost:5173'],
   credentials: true
 }));
 
